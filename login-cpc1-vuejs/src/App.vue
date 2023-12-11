34cbdf41afb615d8df3929ef998cd4c0ecb6ad47
Tốt <totlv99@gmail.com>
2023-12-11 17:30:03 +0700

    mmm

---
+++ login-cpc1-vuejs/src/App.vue
@@ -0,0 +1,20 @@
+<template><LoginComponent /></template>
+
+<script>
+import LoginComponent from './components/login/LoginComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LoginComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
